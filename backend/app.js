c862baa6c12873797f3a76efba6f2470c80ea9f1
harunsaranovic <harunsaranovic@gmail.com>
2019-04-28 15:25:39 +0200

    Database tested - user created (user: admin, pass:admin)

--- backend/app.js
+++ backend/app.js
@@ -22,14 +22,33 @@ mongoose.connect('mongodb://ekipa:OFRaidReekKose3@ds147926.mlab.com:47926/driveu
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
-const User = new Schema({
+const UserSchema = new Schema({
   id: ObjectId,
   username: String,
   password: String
 });
 
+var User = mongoose.model('Users', UserSchema)
+
+//This is how you prepare data for database
+/*
+var userTest = new User({
+  username: 'admin',
+  password: 'admin'
+})
+*/
 
 
-app.get('/', (req, res) => res.send('Backend!'))
+app.get('/', (req, res) => {
+  res.send('Backend!')
+
+  //This is function for inserting the data to database
+  /*
+  userTest.save(function (err, book) {
+      if (err) return console.error(err);
+      console.log(book.name + " saved to bookstore collection.");
+    });
+  */
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
