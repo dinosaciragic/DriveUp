b8af4f0bd69ddaaf0366196df93ed01e5df52194
harunsaranovic <harunsaranovic@gmail.com>
2019-04-28 13:46:19 +0200

    Database added

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,26 @@ const requestHandler = (request, response) => {
   response.end('Node.js Server!')
 }
 
+//mongodb://<dbuser>:<dbpassword>@ds147926.mlab.com:47926/driveup
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://ekipa:passowrd1@ds147926.mlab.com:47926/driveup', {useNewUrlParser: true},function(err)=>{
+        if(err) {
+            console.log('Some problem with the connection ' +err);
+        } else {
+            console.log('The Mongoose connection is ready');
+        }
+  });
+
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+const User = new Schema({
+  id: ObjectId,
+  username: String,
+  password: String
+});
+
 const server = http.createServer(requestHandler)
 
 server.listen(port, (err) => {
